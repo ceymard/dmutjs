e17d405dd6f5a12299584b6e6e6638e41b54440b
Christophe Eymard <christophe.eymard@sales-way.com>
2018-01-23 18:23:15 +0100

    starting mutations as JS

--- src/cli.ts
+++ src/cli.ts
@@ -1,38 +1,38 @@
 #!/usr/bin/env node
-// import {bootstrap} from './database'
-import {fetchLocalMutations, Mutation} from './mutation'
-import {bootstrap, fetchRemoteMutations, MutationRunner} from './database'
-// import ch from 'chalk'
-import * as log from './log'
+// // import {bootstrap} from './database'
+// import {Mutation} from './mutation'
+// import {bootstrap, fetchRemoteMutations, MutationRunner} from './database'
+// // import ch from 'chalk'
+// import * as log from './log'
 
-async function run() {
-  console.log('bootstraping')
-  await bootstrap()
+// async function run() {
+//   console.log('bootstraping')
+//   await bootstrap()
 
-  const local = await fetchLocalMutations()
-  const remotes = await fetchRemoteMutations()
-  var error = false
+//   // const local = await fetchLocalMutations()
+//   const remotes = await fetchRemoteMutations()
+//   var error = false
 
-  const print = Mutation.once(m => {
-    // console.log(
-    //   `${ch.yellowBright(m.module)}:${ch.redBright(m.name)}${m.serie ? ch.greenBright('.' + m.serie) : ''}`,
-    // )
-    for (var e of m.errors) { log.err(e); error = true }
-  })
+//   // const print = Mutation.once(m => {
+//   //   // console.log(
+//   //   //   `${ch.yellowBright(m.module)}:${ch.redBright(m.name)}${m.serie ? ch.greenBright('.' + m.serie) : ''}`,
+//   //   // )
+//   //   for (var e of m.errors) { log.err(e); error = true }
+//   // })
 
-  for (var mut of local) {
-    await mut.up(print)
-  }
+//   // for (var mut of local) {
+//   //   await mut.up(print)
+//   // }
 
-  if (!error) {
-    const runner = new MutationRunner(local, remotes)
-    await runner.mutate()
-  }
+//   // if (!error) {
+//   //   const runner = new MutationRunner(local, remotes)
+//   //   await runner.mutate()
+//   // }
 
-  process.exit(0)
-}
+//   process.exit(0)
+// }
 
-run().catch(e => {
-  console.error(e)
-  process.exit(1)
-})
+// run().catch(e => {
+//   console.error(e)
+//   process.exit(1)
+// })
--- src/mutation.ts
+++ src/mutation.ts
@@ -1,9 +1,5 @@
+
 import * as cr from 'crypto'
-import * as res from 'resolve'
-
-const re_down = /^--\s*!d(?:own)?\(((?:.|\r|\n)*?)^--\)\s*\n|^--\s*!d(?:own)?:?(.*?)$/gim
-const re_split = /^--\s*!.*?$|^--\s*!d(?:own)?\((?:(?:.|\r|\n)*?)^--\)\s*\n/gim
-
 
 /**
  * Memoize un appel d'un get(). À n'utiliser que sur des properties calculées,
@@ -27,269 +23,134 @@ export function memoize(target: any, key: string, descriptor: PropertyDescriptor
 }
 
 
-export enum MutationStatus {
-  UNAPPLIED = 'u',
-  APPLIED = 'a',
-  REMOVED = 'r',
-  UNSYNC = 'u',
+export const re_auto = /^\s*create\s+(\w+)\s+((?:(?:\w+|"[^"]+"|`[^`]+`|\[[^\]]\])\.)*(?:\w+|"[^"]+"|`[^`]+`|\[[^\]]\]))/i
+
+
+export function tpljoin(s: TemplateStringsArray, a: any[]) {
+  const res = [] as string[]
+  for (var i = 0; i < s.length - 1; i++) {
+    res.push(s[i])
+    res.push(a[i].toString())
+  }
+  res.push(s[i])
+  return res.join('').trim()
 }
 
 
 export class Mutation {
+  deps = new Set<Mutation>()
+  children = new Set<Mutation>()
+  statements: string[] = []
+  undo: string[] = []
+  static: boolean = false
 
-  static once<T extends ((mut: Mutation, ...a: any[]) => any)>(fn: T): T {
-    const results = new Map<Mutation, any>()
-    return function(mut: Mutation, ...a: any[]) {
-      if (results.has(mut))
-        return results.get(mut)
-      const res = fn(mut, ...a)
-      results.set(mut, res)
-      return res
-    } as T
+  static depends(...ms: Mutation[]) {
+    return new Mutation()
+      .depends(...ms)
   }
 
-  /**
-   * A registry where all the mutations are stored. It is sorted by mutation name.
-   */
-  public children = new Set<Mutation>()
-  public parents = new Set<Mutation>()
-
-  public serie: number | null = null
-  public errors: string[] = []
-
-  //////////////////////////////////////
-
-  constructor(
-    public name: string,
-    public module: string,
-    public source: string
-  ) {
-    const serie = /(.*?)\.(\d+)$/
-    const match = serie.exec(name)
-    if (match) {
-      this.name = match[1]
-      this.serie = parseInt(match[2])
-    }
+  static get create() {
+    return new Mutation()
   }
 
-  get full_name() {
-    return `${this.module}:${this.name}${this.serie != null ? '.' + this.serie : ''}`
+  static get static() {
+    return new Mutation()
+      .setStatic()
   }
 
-  /**
-   * The hash helps us to determine if a mutation has changed or not.
-   */
+  setStatic() {
+    this.static = true
+    return this
+  }
+
   @memoize
   get hash(): string {
     const hash = cr.createHash('sha256') // this should be enough to avoid collisions
 
     // we have to be smart about the source and remove only the parts we don't want
     // to compare only the code.
-    const amended_src = this.source
-      // We remove single line comments, except if they start with a !, as it has meaning to us.
-      .replace(/--(?!\s*!).*?$/gm, '')
-      // we are not handling recursive comments, and we don't care.
-      .replace(/\/\*((?!\*\/)(.|\r|\n))*?\*\//mg, '')
-      // whitespace should not affect if our file changed or not.
-      .replace(/[\n\r\t\s]/g, ' ')
-      .replace(/ +/g, ' ')
-    hash.update(amended_src)
+    for (var s of [...this.statements, ...this.undo]) {
+      const replaced = s
+        // We remove single line comments, except if they start with a !, as it has meaning to us.
+        .replace(/--(?!\s*!).*?$/gm, '')
+        // we are not handling recursive comments, and we don't care.
+        .replace(/\/\*((?!\*\/)(.|\r|\n))*?\*\//mg, '')
+        // whitespace should not affect if our file changed or not.
+        .replace(/[\n\r\t\s]/g, ' ')
+        .replace(/ +/g, ' ')
+      hash.update(replaced)
+    }
 
     return hash.digest('hex')
   }
 
-  /**
-   * Get all the down statements, in reverse order, ready to be applied.
-   */
-  @memoize
-  get down_statements(): string[] {
-    var match: RegExpMatchArray | null
-    const res = []
-    while (match = re_down.exec(this.source)) {
-      const code = match[1] || match[2]
-      res.push(code.trim())
-    }
-    res.reverse()
-    return res
-  }
+  depends(...ms: Mutation[]) {
+    // Add parents and children.
+    for (var m of ms) {
+      if (this.static && !m.static)
+        throw new Error(`A static mutation can't depend on a non-static one.`)
 
-  @memoize
-  get up_statements(): string[] {
-    return this.source
-      .replace(/\s*--(?!\s*!).*?$/gm, '')
-      // we are not handling recursive comments, and we don't care.
-      .replace(/\/\*((?!\*\/)(?:.|\r|\n))*?\*\//mg, '')
-      .split(re_split)
-      .filter(s => (s||'').trim() !== '')
-      .map(s => s.trim())
-  }
-
-  /**
-   * Runs this mutation and its children
-   */
-  async up(fn: (m: Mutation) => any, visited = new Set<Mutation>(), do_children = true) {
-    if (visited.has(this)) return
-    visited.add(this)
-
-    for (var parent of this.parents)
-      await parent.up(fn, visited, false)
-
-    await fn(this)
-
-    if (do_children)
-      for (var chld of this.children)
-        await chld.up(fn, visited)
-  }
-
-  /**
-   * Removes this mutation along with its children
-   */
-  async down(fn: (m: Mutation) => Promise<any>) {
-    // Check that we haven't already been downed by verifying that
-    // there is no entry in the mutation table
-    for (var chld of this.children)
-      await chld.down(fn)
-
-    await fn(this)
-  }
-
-  computeRequirement(mutations: Mutation[]) {
-
-    var descriptors = [] as string[]
-
-    if (this.serie && this.serie > 1)
-      descriptors.push(`${this.name}.${this.serie - 1}`)
-
-    const re_require = /^--\s*!r(?:equires?)?:?\s*(.*)$/im
-    const match = re_require.exec(this.source)
-    if (match) {
-      descriptors = [...descriptors, ...match[1].split(',').map(r => r.trim())]
+      m.children.add(this)
+      this.deps.add(this)
     }
 
-    const re_req = /^(?:([^:]+):)?([^\.]+)(?:\.(\d+))?$/
+    return this
+  }
 
-    for (var desc of descriptors) {
+  auto(str: TemplateStringsArray, ...a: any[]) {
+    const stmt = tpljoin(str, a)
+    const match = re_auto.exec(stmt)
+    if (match == null)
+      throw new Error(`Unrecognized statement for auto(): ${stmt}`)
+    const type = match[1]
+    const ident = match[2]
+    this.undo.push(`drop ${type} ${ident}`)
+    console.log(`drop ${type} ${ident} -- auto-generated`)
+    this.statements.push(stmt)
+    return this
+  }
 
-      const match = re_req.exec(desc)
-      if (!match) {
-        this.errors.push(`${desc} is not a valid requirement`)
-        return
+  protected up(str: TemplateStringsArray, ...a: any[]) {
+    const stmt = tpljoin(str, a)
+    this.statements.push(stmt)
+    // Devrait renvoyer down.
+    return this
+  }
+
+  down(str: TemplateStringsArray, ...a: any[]) {
+    const stmt = tpljoin(str, a)
+    this.undo.unshift(stmt)
+    const _t = this
+    return {
+      up(str: TemplateStringsArray, ...a: any[]): Mutation {
+        _t.up(str, ...a)
+        return _t
       }
-
-      const module: string = match[1] || this.module
-      const name: string = match[2]
-      const serie: number | null = match[3] !== undefined ? parseInt(match[3]) : null
-
-      // Rebuild the requirement regexp
-      const re_desc = new RegExp(`^${module}:${name}(?:\\/[^\\.]+?)?${serie ? '\\.' + serie : '(?:\\.\\d+)?'}$`)
-      // console.log(re_desc.source)
-      let found = false
-
-      for (var m of mutations) {
-        if (m === this) continue
-
-        // Modules are required directly
-        if (re_desc.test(m.full_name)) {
-          if (this.serie != null && m.serie == null)
-            this.errors.push(`serial migrations cannot depend on non-serial ones (caused by ${m.full_name})`)
-
-          found = true
-          this.parents.add(m)
-          m.children.add(this)
-        }
-      }
-
-      if (!found)
-        this.errors.push(`requirement ${desc} doesn't match any mutation`)
     }
   }
 
+  fn(fn: Function, ...types: string[]) {
 
-}
+    if (fn.length !== types.length - 1)
+      throw new Error(`You muse define the same number of types for your function arguments as well as the return type.`)
 
+    const re_js = /^\s*function\s+\w+\s*\(([^\)]*)\)\s*\{([^]*)\}\s*/igm
 
-export class MutationSet extends Set<Mutation> {
+    const src = fn.toString()
+    const match = re_js.exec(src)
+    if (!match) throw new Error(`Unable to parse function !`)
 
-  private dct: {[name: string]: Mutation} = {}
+    const args = match[1].split(',').map((a, i) => `${a.trim()} ${types[i]}`.replace('...', 'variadic '))
+    console.log(args)
+    const body = match[2].trim()
 
-  constructor(mutations: Mutation[] = []) {
-    super()
-    for (var m of mutations) {
-      this.add(m)
-    }
+    const stmt = `create function ${fn.name}(${args}) returns ${types[types.length - 1]} as $$
+      ${body}
+    $$ language plv8`
+    console.log(stmt)
+    const undo = `drop function ${fn.name}(${types.join(',')})`
+    this.statements.push(stmt)
+    this.undo.unshift(undo)
   }
 
-  add(mut: Mutation) {
-    const r = super.add(mut)
-    this.dct[mut.full_name] = mut
-    return r
-  }
-
-  get(name: string): Mutation | undefined {
-    return this.dct[name]
-  }
-
-  /**
-   * Get our own mutations that have changed in the remote
-   * set. Used to find out which are the mutations we will have
-   * to down.
-   *
-   * @param set The other set
-   */
-  diff(set: MutationSet) {
-    const res = new Set<Mutation>()
-    for (var m of this) {
-      var s = set.get(m.full_name)
-      if (!s || m.hash !== s.hash)
-        res.add(m)
-    }
-    return res
-  }
-
-  intersect(set: MutationSet) {
-    const res = new Set<Mutation>()
-    for (var m of this) {
-      var s = set.get(m.full_name)
-      if (s && m.hash !== s.hash)
-        res.add(m)
-    }
-    return res
-  }
-
-}
-
-
-
-import {getInfos, getScripts} from './utils'
-async function fetchLocal(path?: string): Promise<Mutation[]> {
-  path = path || process.cwd()
-  const infos = await getInfos(path)
-  const name = infos.name
-  var imports = [] as string[]
-
-  var mutations = [] as Mutation[]
-  if (infos.dmut && infos.dmut.import) {
-    imports = infos.dmut.import
-    for (var i of imports) {
-
-      mutations = [...(await fetchLocalMutations(
-        res.sync(i, {basedir: path})
-      )), ...mutations]
-    }
-  }
-
-  for (var s of await getScripts(infos.path)) {
-    var m = new Mutation(s.name, name, s.source)
-    mutations.push(m)
-  }
-
-  return mutations
-}
-
-
-export async function fetchLocalMutations(path?: string): Promise<MutationSet> {
-  const mutations = await fetchLocal(path)
-  for (var m of mutations)
-    m.computeRequirement(mutations)
-  return new MutationSet(mutations)
 }
